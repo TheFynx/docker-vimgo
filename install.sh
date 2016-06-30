363d2e36fc7afe5de071f8799f9b337878f26e39
Levi Smith <levi@fynx.me>
2016-06-30 17:57:14 -0500

    escaping variables in install.sh for vimgo command script

--- install.sh
+++ install.sh
@@ -15,41 +15,41 @@ echo -e '>> Setting up vimgo command, will require elevated privileges'
 sudo cat > /usr/local/bin/vimgo <<EOF
 #!/bin/bash
 
-usage="$(basename "$0") [-h] [-p /path/to/project] -- Small docker wrapper script to run vim-go-ide with project path
+usage="\$(basename "\$0") [-h] [-p /path/to/project] [-b] [-c command] -- Small docker wrapper script to run vim-go-ide with project path
 
 where:
     -h  show this help text
-    -p  set path (default \$(pwd))"
+    -p  set path (default \$(pwd))
     -b  Enter container at Bash instead of Vim
-    -c  Choose command to run in container instead of vim
+    -c  Choose command to run in container instead of vim"
 
-WORKPATH=$(pwd)
+WORKPATH=\$(pwd)
 while getopts ':hpbc:' option; do
-  case "$option" in
-    h) echo "$usage"
+  case "\$option" in
+    h) echo "\$usage"
        exit
        ;;
-    p) WORKPATH=$OPTARG
+    p) WORKPATH=\$OPTARG
        ;;
     b) CMDARG="/bin/bash"
        exit
        ;;
-    c) CMDARG=$OPTARG
+    c) CMDARG=\$OPTARG
        ;;
-    :) printf "missing argument for -%s\n" "$OPTARG" >&2
-       echo "$usage" >&2
+    :) printf "missing argument for -%s\n" "\$OPTARG" >&2
+       echo "\$usage" >&2
        exit 1
        ;;
-   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
-       echo "$usage" >&2
+   \?) printf "illegal option: -%s\n" "\$OPTARG" >&2
+       echo "\$usage" >&2
        exit 1
        ;;
   esac
 done
-shift $((OPTIND - 1))
+shift \$((OPTIND - 1))
 
-if [ $(command -v docker) ]; then
-    docker run --rm -tiv ${WORKPATH}:/project thefynx/vim-go-ide ${CMDARG}
+if [ \$(command -v docker) ]; then
+    docker run --rm -tiv \${WORKPATH}:/project thefynx/vim-go-ide \${CMDARG}
 else
     echo -e 'This program requires Docker, please install docker'
 fi
