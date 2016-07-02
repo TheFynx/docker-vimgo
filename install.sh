eb3d837ec1f83959326fb5d2ef62e621bf2feddf
Levi Smith <levi@fynx.me>
2016-07-01 21:38:09 -0500

    Moving go bin files, adding image option to vimgo for testing local builds, fixing vim lint items on vimrc

--- install.sh
+++ install.sh
@@ -15,21 +15,25 @@ echo -e '>> Setting up vimgo command, will require elevated privileges'
 cat > /tmp/vimgo <<EOF
 #!/bin/bash
 
-usage="\$(basename "\$0") [-h] [-p /path/to/project] [-b] [-c command] -- Small docker wrapper script to run vim-go-ide with project path
+usage="\$(basename "\$0") [-h] [-p /path/to/project] [-b] [-c command] [-i image_name] -- Small docker wrapper script to run vim-go-ide with project path
 
 where:
     -h  show this help text
     -p  set path (default is currently \$(pwd))
     -b  Enter container at Bash instead of Vim
-    -c  Choose command to run in container instead of vim"
+    -c  Choose command to run in container instead of vim
+    -i  For passing a different image name (used for local images)"
 
 WORKPATH=\$(pwd)
-while getopts 'p:c:hb' opt; do
+IMAGE='thefynx/vim-go-ide'
+while getopts 'p:c:i:hb' opt; do
   case "\$opt" in
     h) echo "\$usage"
        exit
        ;;
     p) WORKPATH=\$OPTARG
+       ;;
+    i) IMAGE=\$OPTARG
        ;;
     b) CMDARG="/bin/bash"
        ;;
@@ -48,7 +52,7 @@ done
 shift \$((OPTIND - 1))
 
 if [ \$(command -v docker) ]; then
-    docker run --rm -tiv \${WORKPATH}:/project thefynx/vim-go-ide \${CMDARG}
+    docker run --rm -tiv \${WORKPATH}:/project \${IMAGE} \${CMDARG}
 else
     echo -e 'This program requires Docker, please install docker'
 fi
@@ -56,7 +60,7 @@ EOF
 
 chmod +x /tmp/vimgo
 
-su -c "mv /tmp/vimgo /usr/local/bin/vimgo"
+sudo su -c "mv /tmp/vimgo /usr/local/bin/vimgo"
 
 echo -e '>> Setup complete'
 echo -e '\n'
